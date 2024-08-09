dcd06e7455b60ac422406fe8723c90de1baabd88
Sax Dhiut <mail@iyxeyl.me>
2024-08-09 18:57:17 +0400

    refactor: rename table object and add user type definition
    Renamed the 'paste' table object to 'pastes' for consistency and added a type definition for the User object from the pastes table. This improves code readability and maintainability.

--- lib/schema.ts
+++ lib/schema.ts
@@ -1,8 +1,10 @@
 import type { PasteContent } from "@/lib/types"
 import { jsonb, pgTable, timestamp, uuid } from "drizzle-orm/pg-core"
 
-export const paste = pgTable("pastes", {
+export const pastes = pgTable("pastes", {
 	uuid: uuid("uuid").defaultRandom(),
 	pasteContent: jsonb("content").$type<PasteContent>().notNull(),
 	createdAt: timestamp("created_at").notNull().defaultNow()
 })
+
+export type User = typeof pastes.$inferSelect
