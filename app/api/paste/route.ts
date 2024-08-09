ad60baa6068e6da45230afe3e9e40992c957db2b
Sax Dhiut <mail@iyxeyl.me>
2024-08-09 19:58:04 +0400

    feat: add GET endpoint for retrieving pastes by uuid
    This change introduces a GET endpoint for retrieving pastes using a uuid query parameter. It validates the presence of the uuid and handles cases where the paste is not found, returning appropriate HTTP status codes.

--- app/api/paste/route.ts
+++ app/api/paste/route.ts
@@ -1,5 +1,6 @@
 import { db } from "@/lib/database"
 import { type PasteInsert, pastes } from "@/lib/schema"
+import { eq } from "drizzle-orm"
 import { NextResponse } from "next/server"
 
 export const POST = async (request: Request) => {
@@ -8,4 +9,16 @@ export const POST = async (request: Request) => {
 	await db.insert(pastes).values(pasteContent)
 
 	return NextResponse.json({ success: true })
+}
+
+export const GET = async (request: Request) => {
+	const { searchParams } = new URL(request.url)
+
+	const uuid = searchParams.get("uuid")
+	if (!uuid) return new NextResponse("uuid query param is required", { status: 400 })
+
+	const [paste] = await db.select().from(pastes).where(eq(pastes.uuid, uuid))
+	if (!paste) return new NextResponse("paste not found", { status: 404 })
+
+	return NextResponse.json(paste)
 }
