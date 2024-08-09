f3fdc5c716377a412c93449a3fa1f3bdfc15783f
Sax Dhiut <mail@iyxeyl.me>
2024-08-09 19:57:55 +0400

    feat: add react-query to provider component
    Implemented QueryClientProvider to facilitate server and browser-specific query client management. This enhancement ensures efficient state management and data fetching in the application.

--- components/providers.tsx
+++ components/providers.tsx
@@ -1,15 +1,30 @@
 "use client"
 
 import { Toaster } from "@/components/ui/sonner"
+import { QueryClient, QueryClientProvider, isServer } from "@tanstack/react-query"
 import { ThemeProvider as NextThemesProvider } from "next-themes"
 import type { ThemeProviderProps } from "next-themes/dist/types"
-import * as React from "react"
+
+const makeQueryClient = () => new QueryClient()
+
+let browserQueryClient: QueryClient | undefined = undefined
+
+const getQueryClient = () => {
+	if (isServer) return makeQueryClient()
+
+	if (!browserQueryClient) browserQueryClient = makeQueryClient()
+	return browserQueryClient
+}
 
 export function Providers({ children, ...props }: ThemeProviderProps) {
+	const queryClient = getQueryClient()
+
 	return (
-		<NextThemesProvider {...props}>
-			{children}
-			<Toaster />
-		</NextThemesProvider>
+		<QueryClientProvider client={queryClient}>
+			<NextThemesProvider {...props}>
+				{children}
+				<Toaster />
+			</NextThemesProvider>
+		</QueryClientProvider>
 	)
 }
