8316f405938a1a2a9dff0da7e9cefa71911ed7d0
Sax Dhiut <mail@iyxeyl.me>
2024-08-10 15:43:12 +0400

    feat: add metadata generation for paste pages
    Implemented generateMetadata function to dynamically create metadata for paste pages. Included support for different metadata based on whether the paste exists, is encrypted, or has content.

--- app/[uuid]/page.tsx
+++ app/[uuid]/page.tsx
@@ -1,10 +1,53 @@
 import { PasteDisplay } from "@/components/paste-display"
 import { getPaste } from "@/lib/select"
+import type { Metadata } from "next"
 
 type PageProps = {
 	params: {
 		uuid: string
 	}
+}
+
+export async function generateMetadata({ params: { uuid } }: PageProps) {
+	const [paste] = await getPaste({ uuid })
+
+	if (!paste) {
+		const title = "Paste does not exist or has expired"
+		return {
+			title: title,
+			openGraph: {
+				title: title,
+				type: "website"
+			}
+		} satisfies Metadata
+	}
+
+	if (paste.encrypted) {
+		const title = "Encrypted paste"
+		const description = "This paste is encrypted and cannot be previewed"
+		return {
+			title: title,
+			description: description,
+			openGraph: {
+				title: title,
+				description: description,
+				type: "website"
+			}
+		} satisfies Metadata
+	}
+
+	const title = "Paste"
+	const description = paste.content.length > 64 ? `${paste.content.slice(0, 64)}...` : paste.content
+
+	return {
+		title: title,
+		description: description,
+		openGraph: {
+			title: title,
+			description: description,
+			type: "website"
+		}
+	} satisfies Metadata
 }
 
 export default async function Page({ params: { uuid } }: PageProps) {
--- lib/select.ts
+++ lib/select.ts
@@ -1,13 +1,16 @@
 import { db } from "@/lib/database"
 import { pastes } from "@/lib/schema"
 import { and, eq, gt } from "drizzle-orm"
+import { cache } from "react"
 
 type GetPasteProps = {
 	uuid: string
 }
 
-export const getPaste = async ({ uuid }: GetPasteProps) =>
-	await db
-		.select()
-		.from(pastes)
-		.where(and(eq(pastes.uuid, uuid), gt(pastes.expiresAt, new Date().toISOString())))
+export const getPaste = cache(
+	async ({ uuid }: GetPasteProps) =>
+		await db
+			.select()
+			.from(pastes)
+			.where(and(eq(pastes.uuid, uuid), gt(pastes.expiresAt, new Date().toISOString())))
+)
