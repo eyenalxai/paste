902b78c66cedb1a42061f332ec217e36578e3f86
Sax Dhiut <mail@iyxeyl.me>
2024-08-11 17:11:18 +0400

    refactor: simplify base64 conversion functions
    Replaced manual loops with single-line functions for better readability and performance. This reduces code complexity and enhances maintainability.

--- lib/encode-decode.ts
+++ lib/encode-decode.ts
@@ -3,22 +3,7 @@ export const keyToBase64 = async (key: CryptoKey): Promise<string> => {
 	return arrayBufferToBase64(exportedKey)
 }
 
-export const arrayBufferToBase64 = (buffer: ArrayBuffer) => {
-	let binary = ""
-	const bytes = new Uint8Array(buffer)
-	const len = bytes.byteLength
-	for (let i = 0; i < len; i++) {
-		binary += String.fromCharCode(bytes[i])
-	}
-	return window.btoa(binary)
-}
+export const arrayBufferToBase64 = (buffer: ArrayBuffer) => window.btoa(String.fromCharCode(...new Uint8Array(buffer)))
 
-export const base64ToArrayBuffer = (base64: string): ArrayBuffer => {
-	const binaryString = window.atob(base64)
-	const len = binaryString.length
-	const bytes = new Uint8Array(len)
-	for (let i = 0; i < len; i++) {
-		bytes[i] = binaryString.charCodeAt(i)
-	}
-	return bytes.buffer
-}
+export const base64ToArrayBuffer = (base64: string): ArrayBuffer =>
+	Uint8Array.from(atob(base64), (c) => c.charCodeAt(0)).buffer
