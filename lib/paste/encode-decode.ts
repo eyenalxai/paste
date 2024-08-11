f1f6d2a4d74ac404917f5b440f71a2107684823d
Sax Dhiut <mail@iyxeyl.me>
2024-08-11 17:11:24 +0400

    refactor: remove unused encode-decode functions
    Removed pasteContentToBase64 and pasteContentFromBase64 from encode-decode.ts. These functions were no longer being utilized in the codebase.

--- lib/paste/encode-decode.ts
+++
@@ -1,9 +0,0 @@
-export const pasteContentToBase64 = async (pasteContent: string) => {
-	return {
-		contentBase64: window.btoa(pasteContent)
-	}
-}
-
-export const pasteContentFromBase64 = (contentBase64: string) => {
-	return window.atob(contentBase64)
-}
