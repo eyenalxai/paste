092fd0ed41ae4a4cbf1776eae762002e5f323be5
Sax Dhiut <mail@iyxeyl.me>
2024-08-11 13:49:15 +0400

    feat: add custom font support to opengraph image
    Integrated Roboto Mono font into opengraph images. Reduced font size and adjusted styles to improve legibility and aesthetics.

--- app/[uuid]/opengraph-image.tsx
+++ app/[uuid]/opengraph-image.tsx
@@ -1,3 +1,6 @@
+import * as fs from "node:fs"
+import path from "node:path"
+import { fileURLToPath } from "node:url"
 import type { PastePageProps } from "@/app/[uuid]/page"
 import { getPaste } from "@/lib/select"
 import { ImageResponse } from "next/og"
@@ -18,10 +21,14 @@ export default async function Image({ params: { uuid } }: PastePageProps) {
 
 	if (!paste || paste.encrypted) return null
 
+	const fontData = await fs.promises.readFile(
+		path.join(fileURLToPath(import.meta.url), "../../../public/Roboto-Mono-Regular.woff")
+	)
+
 	return new ImageResponse(
 		<div
 			style={{
-				fontSize: "2rem",
+				fontSize: "1.5rem",
 				fontFamily: "monospace",
 				backgroundColor: "#020817",
 				color: "#f8fafc",
@@ -37,7 +44,14 @@ export default async function Image({ params: { uuid } }: PastePageProps) {
 			{paste.content.length > 800 ? `${paste.content.slice(0, 800)}...` : paste.content}
 		</div>,
 		{
-			...size
+			...size,
+			fonts: [
+				{
+					name: "Roboto Mono",
+					data: fontData,
+					style: "normal"
+				}
+			]
 		}
 	)
 }
