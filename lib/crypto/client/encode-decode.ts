b4aef944ebf8bd91fb38633d85d7228ecbd3d2a3
Sax Dhiut <mail@iyxeyl.me>
2024-08-14 20:58:38 +0400

    refactor: optimize array buffer to base64 conversion
    Simplify the array buffer to base64 string conversion using reduce. This improves readability and performance in both server and client-side code.

--- lib/crypto/client/encode-decode.ts
+++ lib/crypto/client/encode-decode.ts
@@ -5,8 +5,8 @@ export const clientKeyToBase64 = async (key: CryptoKey): Promise<string> => {
 	return clientArrayBufferToBase64(exportedKey)
 }
 
-export const clientArrayBufferToBase64 = (buffer: ArrayBuffer) =>
-	window.btoa(String.fromCharCode(...new Uint8Array(buffer)))
+export const clientArrayBufferToBase64 = (buffer: ArrayBuffer): string =>
+	window.btoa(new Uint8Array(buffer).reduce((acc, byte) => acc + String.fromCharCode(byte), ""))
 
 export const clientBase64ToArrayBuffer = (base64: string): ArrayBuffer =>
 	Uint8Array.from(window.atob(base64), (c) => c.charCodeAt(0)).buffer
--- lib/crypto/server/encode-decode.ts
+++ lib/crypto/server/encode-decode.ts
@@ -6,7 +6,7 @@ export const serverKeyToBase64 = async (key: CryptoKey): Promise<string> => {
 }
 
 export const serverArrayBufferToBase64 = async (buffer: ArrayBuffer) =>
-	btoa(String.fromCharCode(...new Uint8Array(buffer)))
+	btoa(new Uint8Array(buffer).reduce((acc, byte) => acc + String.fromCharCode(byte), ""))
 
 export const serverBase64ToArrayBuffer = async (base64: string) =>
 	Uint8Array.from(atob(base64), (c) => c.charCodeAt(0)).buffer
