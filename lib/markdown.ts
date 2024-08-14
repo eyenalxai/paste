03736037e8b24b8d24f336f6a4f604c8f86c20a8
Sax Dhiut <mail@iyxeyl.me>
2024-08-14 13:49:44 +0400

    fix: handle case-insensitivity for markdown extension
    Previously, the function did not account for different cases in the "markdown" extension, leading to potential mismatches. This change ensures consistency by converting the extension to lowercase before comparison.

--- lib/markdown.ts
+++ lib/markdown.ts
@@ -6,7 +6,7 @@ type WrapProps = {
 
 export const wrapInMarkdown = ({ syntax, extension, content }: WrapProps) => {
 	if (extension !== undefined) {
-		return extension === "markdown" ? content : `\`\`\`${extension}\n${content}\n\`\`\``
+		return extension.toLowerCase() === "markdown" ? content : `\`\`\`${extension}\n${content}\n\`\`\``
 	}
 
 	if (syntax === "markdown") {
