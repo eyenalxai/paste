d1d7831724bd4124e8b660f235e8acff26cb6c56
Sax Dhiut <mail@iyxeyl.me>
2024-08-15 00:36:45 +0400

    feat: add metadata generation for paste pages
    Implemented the generateMetadata function to dynamically create metadata for paste pages. This includes generating titles and descriptions based on the paste's existence, encryption, and content length.

--- app/[uuidWithExt]/page.tsx
+++ app/[uuidWithExt]/page.tsx
@@ -1,9 +1,11 @@
 import { PasteContainer } from "@/components/paste-container"
 import { PasteDisplay } from "@/components/paste-display"
+import { env } from "@/lib/env.mjs"
 import { wrapInMarkdown } from "@/lib/markdown"
 import { getPaste } from "@/lib/select"
 import { extractUuidAndExtension } from "@/lib/uuid-extension"
 import { all } from "lowlight"
+import type { Metadata } from "next"
 import { MDXRemote } from "next-mdx-remote/rsc"
 import { permanentRedirect } from "next/navigation"
 import rehypeHighlight from "rehype-highlight"
@@ -15,6 +17,55 @@ export type PastePageProps = {
 	searchParams: {
 		key: string
 	}
+}
+
+export async function generateMetadata({ params: { uuidWithExt }, searchParams: { key } }: PastePageProps) {
+	const frontendUrl = env.NEXT_PUBLIC_FRONTEND_URL
+
+	const [uuid] = extractUuidAndExtension(uuidWithExt)
+
+	const { decryptedContent, paste } = await getPaste({ uuid, key })
+
+	if (!paste) {
+		const title = "Paste does not exist or has expired"
+		return {
+			title: title,
+			openGraph: {
+				title: title,
+				url: new URL(`${frontendUrl}/${uuid}`),
+				type: "website"
+			}
+		} satisfies Metadata
+	}
+
+	if (paste.ivClientBase64) {
+		const title = "Encrypted paste"
+		const description = "This paste is encrypted and cannot be previewed"
+		return {
+			title: title,
+			description: description,
+			openGraph: {
+				title: title,
+				description: description,
+				url: new URL(`${frontendUrl}/${uuid}`),
+				type: "website"
+			}
+		} satisfies Metadata
+	}
+
+	const title = "Paste"
+	const description = decryptedContent.length > 64 ? `${decryptedContent.slice(0, 64)}...` : decryptedContent
+
+	return {
+		title: title,
+		description: description,
+		openGraph: {
+			title: title,
+			description: description,
+			url: new URL(`${frontendUrl}/${uuid}`),
+			type: "website"
+		}
+	} satisfies Metadata
 }
 
 export default async function Page({ params: { uuidWithExt }, searchParams: { key } }: PastePageProps) {
