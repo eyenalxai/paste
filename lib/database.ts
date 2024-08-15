7a54b89b504fa3942c1e9735c33b8544afddd37f
Sax Dhiut <mail@iyxeyl.me>
2024-08-15 12:03:12 +0400

    refactor: increase database connection pool size
    Increased the maximum number of connections in the pool from 1 to 10. This change aims to improve the application's ability to handle concurrent database requests.

--- lib/database.ts
+++ lib/database.ts
@@ -3,6 +3,6 @@ import { env } from "@/lib/env.mjs"
 import { drizzle } from "drizzle-orm/postgres-js"
 import postgres from "postgres"
 
-const pool = postgres(env.DATABASE_URL, { max: 1 })
+const pool = postgres(env.DATABASE_URL, { max: 10 })
 
 export const db = drizzle(pool)
