c61fdd00843d7edcc55638f2bb1925e9e8b54b86
Sax Dhiut <mail@iyxeyl.me>
2024-08-17 21:30:24 +0400

    feat: add form data parsing using zod-form-data
    Introduce parseZodFormDataSchema to handle FormData parsing with zod-form-data. Update route to utilize this function for better error handling and validation.

--- app/api/paste/route.ts
+++ app/api/paste/route.ts
@@ -6,15 +6,18 @@ import { getExpiresAt } from "@/lib/date"
 import { getPasteSyntax } from "@/lib/syntax/detect"
 import { buildPasteUrl } from "@/lib/url"
 import { BackendSchema } from "@/lib/zod/form/backend"
+import { parseZodFormDataSchema } from "@/lib/zod/parse"
 import { NextResponse } from "next/server"
+import type { z } from "zod"
 
 export const POST = async (request: Request) => {
 	const badContentLengthResponse = await contentLength(request)
 	if (badContentLengthResponse) return badContentLengthResponse
 
-	const { ivClient, contentBlob, oneTime, expiresAfter, contentType, syntax } = BackendSchema.parse(
-		await request.formData()
-	)
+	const formPasteResult = parseZodFormDataSchema<z.infer<typeof BackendSchema>>(BackendSchema, await request.formData())
+	if (formPasteResult.isErr()) return new NextResponse(formPasteResult.error, { status: 400 })
+
+	const { ivClient, contentBlob, oneTime, expiresAfter, contentType, syntax } = formPasteResult.value
 
 	const content = await contentBlob.text()
 
--- lib/zod/parse.ts
+++ lib/zod/parse.ts
@@ -1,5 +1,6 @@
 import { type Result, err, ok } from "neverthrow"
-import { ZodError, type ZodSchema } from "zod"
+import { ZodError, type ZodSchema, type z } from "zod"
+import { formData } from "zod-form-data"
 
 export const parseZodSchema = <T>(schema: ZodSchema<T>, data: unknown): Result<T, string> => {
 	try {
@@ -9,5 +10,18 @@ export const parseZodSchema = <T>(schema: ZodSchema<T>, data: unknown): Result<T
 			return err(error.errors.map((e) => e.message).join(", "))
 		}
 		return err("An unexpected error during schema validation occurred")
+	}
+}
+
+export const parseZodFormDataSchema = <T>(schema: z.ZodTypeAny, data: FormData): Result<T, string> => {
+	try {
+		const formSchema = formData(schema)
+		const formDataObject = formSchema.parse(data)
+		return ok(formDataObject)
+	} catch (error) {
+		if (error instanceof ZodError) {
+			return err(error.errors.map((e) => e.message).join(", "))
+		}
+		return err("An unexpected error during form data schema validation occurred")
 	}
 }
