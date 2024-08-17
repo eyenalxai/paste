0dcf11848bd6f1070ffc6e1f110b80e17d47e32c
Sax Dhiut <mail@iyxeyl.me>
2024-08-17 22:25:37 +0400

    feat: auto-open URL if not one-time
    Updated the paste form logic to automatically open the URL in a new tab if the form is not marked as one-time. This streamlines the user experience by reducing manual steps.

--- lib/hooks/paste-form.tsx
+++ lib/hooks/paste-form.tsx
@@ -34,18 +34,20 @@ export const usePasteForm = () => {
 				.andThen((url) => copyToClipboard(url).andThen(() => ok(url)))
 				.match(
 					(url) => {
-						toast.info(
-							<div className={cn("flex", "flex-row", "gap-4", "justify-between", "items-center", "w-full")}>
-								<div>URL copied to clipboard</div>
-								{!formData.encrypted && (
-									<Button asChild variant={"outline"} className={cn("h-8")}>
-										<a target={"_blank"} rel="noopener noreferrer" href={url}>
-											Open
-										</a>
-									</Button>
-								)}
-							</div>
-						)
+						!formData.oneTime
+							? window.open(url, "_blank")
+							: toast.info(
+									<div className={cn("flex", "flex-row", "gap-4", "justify-between", "items-center", "w-full")}>
+										<div>URL copied to clipboard</div>
+										{!formData.encrypted && !formData.oneTime && (
+											<Button asChild variant={"outline"} className={cn("h-8")}>
+												<a target={"_blank"} rel="noopener noreferrer" href={url}>
+													Open
+												</a>
+											</Button>
+										)}
+									</div>
+								)
 
 						methods.reset({
 							content: "",
