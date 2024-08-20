dc383fa76a3e12b5b3bd7b56ac537306a2d0f639
Sax Dhiut <mail@iyxeyl.me>
2024-08-20 19:27:45 +0400

    refactor: use class-variance-authority for alert styling
    This change simplifies the handling of variant styles for alerts by using class-variance-authority. It removes redundant code and makes the component more maintainable.

--- components/error/paste-alert.tsx
+++ components/error/paste-alert.tsx
@@ -1,5 +1,6 @@
 import { Alert, AlertDescription, AlertTitle } from "@/components/ui/alert"
 import { cn } from "@/lib/utils"
+import { cva } from "class-variance-authority"
 import { CircleAlertIcon } from "lucide-react"
 
 type PasteAlertProps = {
@@ -8,27 +9,39 @@ type PasteAlertProps = {
 	variant: "destructive" | "warning"
 }
 
-export const PasteAlert = ({ title, description, variant }: PasteAlertProps) => {
-	const colors = {
-		destructive: {
-			bg: ["bg-red-200", "dark:bg-red-900"],
-			border: ["border-red-500", "dark:border-red-500"],
-			stroke: ["stroke-red-900", "dark:stroke-red-200"],
-			text: ["text-red-900", "dark:text-red-200"]
-		},
-		warning: {
-			bg: ["bg-yellow-200", "dark:bg-yellow-900"],
-			border: ["border-yellow-500", "dark:border-yellow-500"],
-			stroke: ["stroke-yellow-900", "dark:stroke-yellow-200"],
-			text: ["text-yellow-900", "dark:text-yellow-200"]
+const pasteAlertVariants = cva("", {
+	variants: {
+		variant: {
+			destructive: cn(
+				"bg-red-200",
+				"dark:bg-red-900",
+				"border-red-500",
+				"dark:border-red-500",
+				"text-red-900",
+				"dark:text-red-200",
+				"[&>svg]:stroke-red-900",
+				"dark:[&>svg]:stroke-red-200"
+			),
+			warning: cn(
+				"bg-yellow-200",
+				"dark:bg-yellow-900",
+				"border-yellow-500",
+				"dark:border-yellow-500",
+				"text-yellow-900",
+				"dark:text-yellow-200",
+				"[&>svg]:stroke-yellow-900",
+				"dark:[&>svg]:stroke-yellow-200"
+			)
 		}
-	} as const
+	}
+})
 
+export const PasteAlert = ({ title, description, variant }: PasteAlertProps) => {
 	return (
-		<Alert variant="destructive" className={cn(colors[variant].bg, colors[variant].border)}>
-			<CircleAlertIcon className={cn("size-4", colors[variant].stroke)} />
-			<AlertTitle className={cn(colors[variant].text)}>{title}</AlertTitle>
-			<AlertDescription className={cn(colors[variant].text)}>{description}</AlertDescription>
+		<Alert className={cn(pasteAlertVariants({ variant }))}>
+			<CircleAlertIcon className="size-4" />
+			<AlertTitle>{title}</AlertTitle>
+			<AlertDescription>{description}</AlertDescription>
 		</Alert>
 	)
 }
