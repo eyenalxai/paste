52ead122b861e801e16434a406f6711879439595
Sax Dhiut <mail@iyxeyl.me>
2024-08-21 12:28:09 +0400

    refactor: replace tab characters in text with spaces
    This change enhances text processing by replacing tab characters with two spaces in PreviewImageContainer. This ensures consistent text formatting across different parts of the application.

--- components/preview-image-container.tsx
+++ components/preview-image-container.tsx
@@ -3,41 +3,45 @@ type PreviewImageContainerProps = {
 	text: string
 }
 
-export const PreviewImageContainer = ({ title, text }: PreviewImageContainerProps) => (
-	<div
-		style={{
-			fontSize: "2rem",
-			fontFamily: "monospace",
-			backgroundColor: "#020817",
-			color: "#f8fafc",
-			width: "100%",
-			height: "100%",
-			display: "flex",
-			flexDirection: "column",
-			alignItems: "flex-start",
-			justifyContent: "space-between",
-			padding: "1rem",
-			whiteSpace: "pre-wrap"
-		}}
-	>
+export const PreviewImageContainer = ({ title, text }: PreviewImageContainerProps) => {
+	const processedText = text.replace(/\t/g, "  ")
+
+	return (
 		<div
 			style={{
-				backgroundImage: "linear-gradient(0deg, #020817 0%, #fff 100%)",
-				backgroundClip: "text",
-				color: "transparent",
-				display: "block",
-				height: "100%"
+				fontSize: "2rem",
+				fontFamily: "monospace",
+				backgroundColor: "#020817",
+				color: "#f8fafc",
+				width: "100%",
+				height: "100%",
+				display: "flex",
+				flexDirection: "column",
+				alignItems: "flex-start",
+				justifyContent: "space-between",
+				padding: "1rem",
+				whiteSpace: "pre-wrap"
 			}}
 		>
-			{text.length > 800 ? `${text.slice(0, 800)}...` : text}
+			<div
+				style={{
+					backgroundImage: "linear-gradient(0deg, #020817 0%, #fff 100%)",
+					backgroundClip: "text",
+					color: "transparent",
+					display: "block",
+					height: "100%"
+				}}
+			>
+				{processedText.length > 800 ? `${processedText.slice(0, 800)}...` : processedText}
+			</div>
+			<div
+				style={{
+					fontSize: "5rem",
+					color: "white"
+				}}
+			>
+				{title}
+			</div>
 		</div>
-		<div
-			style={{
-				fontSize: "5rem",
-				color: "white"
-			}}
-		>
-			{title}
-		</div>
-	</div>
-)
+	)
+}
