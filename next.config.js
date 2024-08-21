d348121798f95e81ccb31c029a0f4574dd300d24
Sax Dhiut <mail@iyxeyl.me>
2024-08-21 21:50:57 +0400

    refactor: switch from rewrites to redirects in config
    Updated next.config.js to use redirects instead of rewrites for certain routes. This change ensures client requests are redirected correctly and reflects better intent of the routing logic.

--- next.config.js
+++ next.config.js
@@ -1,35 +1,25 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-	async rewrites() {
-		return {
-			beforeFiles: [
-				{
-					source: "/:id*/raw",
-					destination: "/api/paste/:id*/raw"
-				},
-				{
-					source: "/",
-					destination: "/api/cli",
-					has: [
-						{
-							type: "header",
-							key: "content-type",
-							value: "multipart/form-data.*"
-						}
-					]
-				},
-				{
-					source: "/(.*)",
-					destination: "https://:1",
-					has: [
-						{
-							type: "host",
-							value: "http://(.*)"
-						}
-					]
-				}
-			]
-		}
+	async redirects() {
+		return [
+			{
+				source: "/:id*/raw",
+				destination: "/api/paste/:id*/raw",
+				permanent: true
+			},
+			{
+				source: "/",
+				destination: "/api/cli",
+				has: [
+					{
+						type: "header",
+						key: "content-type",
+						value: "multipart/form-data.*"
+					}
+				],
+				permanent: true
+			}
+		]
 	}
 }
 
